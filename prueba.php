c8bf77512d6285f6c543936b7a62a7a6665b1803
Jonathan <jonathan13779@gmail.com>
2022-09-14 12:41:34 +0000

    test

---
+++ prueba.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use Jonathan13779\Framework\Test;
+
+$test = new Test;
+$test->test();
---
+++ src/Test.php
@@ -0,0 +1,8 @@
+<?php
+namespace Jonathan13779\Framework;
+
+class Test{
+    public function test(){
+        echo 'hola mundo';
+    }
+}
